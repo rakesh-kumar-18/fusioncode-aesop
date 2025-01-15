c8cecfaa8515059152ae4e7b6cc8eee44d27d8c1
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2025-01-15 22:09:26 +0530

    add quote section

---
+++ src/components/QuoteSection.jsx
@@ -0,0 +1,14 @@
+const QuoteSection = () => {
+    return (
+        <div className="bg-neutral-50 text-center py-16">
+            <blockquote className="text-2xl md:text-4xl font-serif font-light text-black">
+                &quot;The manner of giving is worth more than the gift.&quot;
+            </blockquote>
+            <cite className="block mt-4 text-lg font-semibold text-gray-700">
+                Pierre Corneille
+            </cite>
+        </div>
+    );
+};
+
+export default QuoteSection;
